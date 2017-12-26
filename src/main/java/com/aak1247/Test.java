07ff0bb7848799a7265aa1eb5f5586bb95a1facd
aak1247 <aak1247@126.com>
2017-12-26 14:39:11 +0800

    initial state

---
+++ src/main/java/com/aak1247/Test.java
@@ -0,0 +1,100 @@
+package com.aak1247;
+
+import org.omg.CORBA.OBJ_ADAPTER;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class Test {
+
+    public static void main(String args[]) {
+        Yaml yaml = new Yaml();
+        URL url = Test.class.getClassLoader().getResource("test.yml");
+        if (url != null){
+            try {
+                Map map = (Map)yaml.load(new FileInputStream(url.getFile()));
+                Test test = new Test();
+                System.out.println( "result: " + test.getStringFromYaml(map, "export.tables.docx.1vs1.index"));
+                Object temp = test.getObjFromMap(map,"export.tables.docx.1vs1.image_a");
+                System.out.println( "result: " + temp.toString());
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getStringFromYaml(Map map, String name) {
+        String listPattern = ".*\\[\\d*]";
+        String[] nameList = name.split("\\.");
+        int count = 0;
+        String curKey = nameList[0];
+        Object curValue = null;
+        while (count < nameList.length){
+            if (Pattern.matches(listPattern, curKey)){
+                //检验本次是否为数组
+                String[] curKeyList = curKey.split("(\\[|])");
+                String curKeyName = curKeyList[0];
+                int index = Integer.parseInt(curKeyList[1]);
+                //取到数组
+                curValue = map.get(curKeyName);
+                //取到数组内元素, 放入map
+                //不一定是map map = (Map) ((List)curValue).get(index);
+                curValue = ((List)curValue).get(index);
+                if (curValue instanceof String){
+                    return (String) curValue;
+                }
+                curKey = nameList[++count];
+                map = (Map)curValue;
+            }
+            //取下一级元素
+            curValue = map.get(curKey);
+            if (curValue instanceof Map){
+                map = (Map) curValue;
+                curKey = nameList[++count];
+            }else if (curValue instanceof String){
+                return (String)curValue;
+            }else return curValue.toString();
+        }
+        return (String) curValue;
+    }
+
+    public Object getObjFromMap(Map map, String name) {
+        String listPattern = ".*\\[\\d*]";
+        String[] nameList = name.split("\\.");
+        int count = 0;
+        String curKey = nameList[0];
+        Object curValue = null;
+        while (count < nameList.length){
+            if (Pattern.matches(listPattern, curKey)){
+                //检验本次是否为数组
+                String[] curKeyList = curKey.split("(\\[|])");
+                String curKeyName = curKeyList[0];
+                int index = Integer.parseInt(curKeyList[1]);
+                //取到数组
+                curValue = map.get(curKeyName);
+                //取到数组内元素, 放入map
+                //不一定是map map = (Map) ((List)curValue).get(index);
+                curValue = ((List)curValue).get(index);
+                if (curValue instanceof String){
+                    return curValue;
+                }
+                curKey = nameList[++count];
+                map = (Map)curValue;
+            }
+            //取下一级元素
+            curValue = map.get(curKey);
+            if (curValue instanceof Map){
+                map = (Map) curValue;
+                curKey = nameList[++count];
+            }else if (curValue instanceof String){
+                return curValue;
+            }else return curValue;
+        }
+        return  curValue;
+    }
+}
